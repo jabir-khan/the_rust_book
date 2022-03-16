4eff68aec2019b04e2b0ca50e184bd76f97c7cb3
Jabir Khan <jabirkhan@Jabirs-MacBook-Pro.local>
2022-03-16 12:11:29 +0545

    chapter 1

---
+++ Chapter_1_hello_world/target/debug/Chapter_1_hello_world.d
@@ -0,0 +1,1 @@
+/Users/jabirkhan/Desktop/learning/rust/the_rust_book/Chapter_1_hello_world/target/debug/Chapter_1_hello_world: /Users/jabirkhan/Desktop/learning/rust/the_rust_book/Chapter_1_hello_world/src/main.rs
---
+++ Chapter_1_hello_world/target/debug/deps/Chapter_1_hello_world-01ae9062e852e251.d
@@ -0,0 +1,5 @@
+/Users/jabirkhan/Desktop/learning/rust/the_rust_book/Chapter_1_hello_world/target/debug/deps/Chapter_1_hello_world-01ae9062e852e251: src/main.rs
+
+/Users/jabirkhan/Desktop/learning/rust/the_rust_book/Chapter_1_hello_world/target/debug/deps/Chapter_1_hello_world-01ae9062e852e251.d: src/main.rs
+
+src/main.rs:
